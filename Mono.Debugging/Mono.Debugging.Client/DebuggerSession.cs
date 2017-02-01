68c53aad16bfcd66636c652a6507d3e8389fee0d
Arseny Chernyaev <arseny.chernyaev@jetbrains.com>
2017-02-01 13:20:47 +0300

    mute breakpoint insertion exceptions

--- Mono.Debugging/Mono.Debugging.Client/DebuggerSession.cs
+++ Mono.Debugging/Mono.Debugging.Client/DebuggerSession.cs
@@ -1283,7 +1283,7 @@ namespace Mono.Debugging.Client
 		{
 			var s = GetBreakEventStatus (be);
 			if (s == BreakEventStatus.BindError || s == BreakEventStatus.Invalid)
-				OnDebuggerOutput (true, GetBreakEventErrorMessage (be) + ": " + GetBreakEventStatusMessage (be) + "\n");
+				OnDebuggerOutput (false, GetBreakEventErrorMessage (be) + ": " + GetBreakEventStatusMessage (be) + "\n");
 			Breakpoints.NotifyStatusChanged (be);
 		}
 		
