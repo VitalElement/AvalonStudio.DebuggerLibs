f1e727f15811efe4f700273e693e20c344906083
Arseny Chernyaev <arseny.chernyaev@jetbrains.com>
2017-02-12 19:01:27 +0300

    created OnAttach using ProcessInfo

---
+++ Mono.Debugging/Mono.Debugging.Client/ClrProcessInfo.cs
@@ -0,0 +1,18 @@
+﻿namespace Mono.Debugging.Client
+{
+	public class ClrProcessInfo: ProcessInfo
+	{
+		private string runtime;
+
+		public ClrProcessInfo (long id, string name, string runtime) : base (id, name)
+		{
+			this.runtime = runtime;
+		}
+
+		public string Runtime {
+			get {
+				return runtime;
+			}
+		}
+	}
+}
