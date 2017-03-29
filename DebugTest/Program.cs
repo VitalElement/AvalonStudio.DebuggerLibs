8a275f87b8eab46b2890b7d36fff7d7ab4187ab1
danwalmsley <dan@walms.co.uk>
2017-03-29 17:03:19 +0100

    guessing default debugging options

--- DebugTest/Program.cs
+++ DebugTest/Program.cs
@@ -1,4 +1,5 @@
-﻿using Mono.Debugging.Win32;
+﻿using Mono.Debugging.Client;
+using Mono.Debugging.Win32;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -14,7 +15,19 @@ namespace DebugTest
         {
             var session = new CoreClrDebuggerSession(Path.GetInvalidPathChars(), "C:\\Program Files\\dotnet\\shared\\Microsoft.NETCore.App\\1.1.1\\dbgshim.dll");
 
-            session.Run(new Mono.Debugging.Client.DebuggerStartInfo() { }, new Mono.Debugging.Client.DebuggerSessionOptions() { });
+            var startInfo = new DebuggerStartInfo() {
+
+                UseExternalConsole = true,
+                CloseExternalConsoleOnExit = true
+            };
+
+            var sessionOptions = EvaluationOptions.DefaultOptions.Clone();
+
+            sessionOptions.EllipsizeStrings = false;
+            sessionOptions.GroupPrivateMembers = false;
+            sessionOptions.EvaluationTimeout = 1000;
+
+            session.Run( startInfo, new DebuggerSessionOptions() { EvaluationOptions = sessionOptions });
         }
     }
 }
