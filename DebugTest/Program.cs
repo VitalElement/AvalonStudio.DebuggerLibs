75d3737b7d7cc9d825277ce9fd5a6c58b561dd7a
danwalmsley <dan@walms.co.uk>
2017-03-29 16:45:12 +0100

    added a console test app.

---
+++ DebugTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using Mono.Debugging.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DebugTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var session = new CoreClrDebuggerSession(Path.GetInvalidPathChars(), "C:\\Program Files\\dotnet\\shared\\Microsoft.NETCore.App\\1.1.1\\dbgshim.dll");
+
+            session.Run(new Mono.Debugging.Client.DebuggerStartInfo() { }, new Mono.Debugging.Client.DebuggerSessionOptions() { });
+        }
+    }
+}
