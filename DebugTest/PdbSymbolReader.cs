78b61b1eb94db0a7750a3fbfc8dc372c7cb65bc1
danwalmsley <dan@walms.co.uk>
2017-03-31 10:12:06 +0100

    initial scaffolding of pdb symbol reader.

---
+++ DebugTest/PdbSymbolReader.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Diagnostics.SymbolStore;
+
+namespace DebugTest
+{
+    public class PdbSymbolReader : ISymbolReader
+    {
+        public SymbolToken UserEntryPoint => throw new NotImplementedException();
+
+        public ISymbolDocument GetDocument(string url, Guid language, Guid languageVendor, Guid documentType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ISymbolDocument[] GetDocuments()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ISymbolVariable[] GetGlobalVariables()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ISymbolMethod GetMethod(SymbolToken method)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ISymbolMethod GetMethod(SymbolToken method, int version)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ISymbolMethod GetMethodFromDocumentPosition(ISymbolDocument document, int line, int column)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ISymbolNamespace[] GetNamespaces()
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte[] GetSymAttribute(SymbolToken parent, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ISymbolVariable[] GetVariables(SymbolToken parent)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
