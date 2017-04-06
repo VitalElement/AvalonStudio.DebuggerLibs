aabfde501dac331d219b32d12fbf4ef2a8730647
danwalmsley <dan@walms.co.uk>
2017-04-06 15:02:22 +0100

    Add a stub for TypeLibTypeAttribute so the lib will compile on netcore 2.

---
+++ CorApi/DotnetStubs/TypeLibTypeAttribute.cs
@@ -0,0 +1,14 @@
+﻿#if NETCOREAPP2_0
+using System;
+
+namespace System.Runtime.InteropServices
+{
+    internal class TypeLibTypeAttribute : Attribute
+    {
+        public TypeLibTypeAttribute(short value)
+        {
+
+        }
+    }
+}
+#endif
