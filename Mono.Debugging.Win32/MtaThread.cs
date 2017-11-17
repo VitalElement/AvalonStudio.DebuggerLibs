e72664b1dd577092b3ec033a84e1b7c6b76267f8
Unknown <dan@walms.co.uk>
2017-11-17 22:38:06 +0000

    fix mta thread.

--- Mono.Debugging.Win32/MtaThread.cs
+++ Mono.Debugging.Win32/MtaThread.cs
@@ -5,6 +5,33 @@ using System.Threading.Tasks;
 
 namespace Mono.Debugging.Win32
 {
+	public class CustomSyncContext : SynchronizationContext
+	{
+		private JobRunner _runner;
+
+		public CustomSyncContext (JobRunner runner)
+		{
+			_runner = runner;
+		}
+
+		public override void Send(SendOrPostCallback d, object state)
+		{
+			if (Thread.CurrentThread == _runner.MainThread)
+			{
+				d(state);
+			}
+			else
+			{
+				_runner.InvokeAsync(() => d(state)).Wait();
+			}
+		}
+
+		public override void Post(SendOrPostCallback d, object state)
+		{
+			_runner.InvokeAsync(() => d(state));
+		}
+	}
+
 	public static class MtaThread
 	{
 		static readonly AutoResetEvent wordDoneEvent = new AutoResetEvent(false);
@@ -13,13 +40,20 @@ namespace Mono.Debugging.Win32
 		static Thread workThread;
 		static Exception workError;
 		static readonly object threadLock = new object();
-		static JobRunner runner = new JobRunner();
+		static JobRunner runner;
 
 		public static R Run<R>(Func<R> ts, int timeout = 15000)
 		{
-			if (Thread.CurrentThread != runner.MainThread)
+			if (AvalonStudio.Platforms.Platform.PlatformIdentifier == AvalonStudio.Platforms.PlatformID.Win32NT)
 			{
-				return ts();
+				if (Thread.CurrentThread.GetApartmentState() == ApartmentState.MTA)
+				{
+					return ts();					
+				}
+			}
+			else if(Thread.CurrentThread == runner.MainThread)
+			{
+				return ts();				
 			}
 
 			R res = default(R);
@@ -32,7 +66,15 @@ namespace Mono.Debugging.Win32
 
 		public static void Run(Action ts, int timeout = 15000)
 		{
-			if (Thread.CurrentThread != workThread)
+			if (AvalonStudio.Platforms.Platform.PlatformIdentifier == AvalonStudio.Platforms.PlatformID.Win32NT)
+			{
+				if (Thread.CurrentThread.GetApartmentState() == ApartmentState.MTA)
+				{
+					ts();
+					return;
+				}
+			}
+			else if(Thread.CurrentThread == runner.MainThread)
 			{
 				ts();
 				return;
@@ -47,6 +89,12 @@ namespace Mono.Debugging.Win32
 					if (workThread == null)
 					{
 						workThread = new Thread(MtaRunner);
+
+						if (AvalonStudio.Platforms.Platform.PlatformIdentifier == AvalonStudio.Platforms.PlatformID.Win32NT)
+						{
+							workThread.SetApartmentState(ApartmentState.MTA);
+						}
+
 						workThread.Name = "Win32 Debugger MTA Thread";
 						workThread.IsBackground = true;
 						workThread.Start();
@@ -59,6 +107,9 @@ namespace Mono.Debugging.Win32
 
 		static void MtaRunner()
 		{
+			runner = new JobRunner();
+
+			SynchronizationContext.SetSynchronizationContext(new CustomSyncContext(runner));
 			runner.RunLoop(new CancellationToken());
 		}
 	}
